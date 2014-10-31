2a06e49b3b69938b56c7d052c43265bddf6281bf
Sebastian Eichler <sebastian.m.eichler@gmail.com>
2014-10-31 14:44:19 -0500

    100 pt complete

--- 100pt.py
+++ 100pt.py
@@ -13,7 +13,8 @@ root = Tk()
 # Create our drawpad and oval
 drawpad = Canvas(root, width=480,height=320, background='white')
 oval = drawpad.create_oval(160,160,320,320, fill="red")
-
+left = -10
+right = 10
 class MyApp:
 	def __init__(self, parent):
 	        # Make sure the drawpad is accessible from inside the function
@@ -21,6 +22,8 @@ class MyApp:
 		self.myParent = parent  
 		self.myContainer1 = Frame(parent)
 		self.myContainer1.pack()
+		self.myContainer2 = Frame(parent)
+		self.myContainer2.pack()
 		
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="Left", background= "green")
@@ -28,13 +31,11 @@ class MyApp:
 		
 	        # Add a second button!
 				
-		
-						
-		# "Bind" an action to the first button												
+		self.button2 = Button(self.myContainer2)
+		self.button2.configure(text="Right", background= "lightblue")
+		self.button2.grid(row=0,column=1)												
 		self.button1.bind("<Button-1>", self.button1Click)
-		# Create the code to bind an action to the second button
-		# Do not change "<Button-1>"
-		 
+		self.button2.bind("<Button-1>", self.button2Click)
 		  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
@@ -46,9 +47,24 @@ class MyApp:
                 # "global" makes sure that we can access our oval and our drawpad
 		global oval
 		global drawpad
-	
+		global left
+	        x1, y1, x2, y2 = drawpad.coords(oval)
+	        drawpad.move(oval,left,0)
+	        if x2 > drawpad.winfo_width(): 
+	             drawpad.move(oval,-drawpad.winfo_width(),0)
+	        if x2 < 0:
+	             drawpad.move(oval,drawpad.winfo_width(),0)
 	# Add the event handler for the second button to make it move right!
-	
+	def button2Click(self, event):
+	        global oval
+	        global drwapad
+	        global right
+	        x1, y1, x2, y2 = drawpad.coords(oval)
+	        drawpad.move(oval,right,0)
+	        if x1 > drawpad.winfo_width(): 
+	             drawpad.move(oval,-drawpad.winfo_width(),0)
+	        if x1 < 0:
+	             drawpad.move(oval,drawpad.winfo_width(),0)
 		
 myapp = MyApp(root)
 root.mainloop()
