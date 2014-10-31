7b424f2396588c1099c3e025a522dd75df47325b
Sebastian Eichler <sebastian.m.eichler@gmail.com>
2014-10-31 14:21:16 -0500

    85 pt
    Button wont change color, but everything looks right...

--- 85pt.py
+++ 85pt.py
@@ -16,6 +16,8 @@ class MyApp:
 		self.myParent = parent  ### (7) remember my parent, the root
 		self.myContainer1 = Frame(parent)
 		self.myContainer1.pack()
+		self.myContainer2 = Frame(parent)
+		self.myContainer2.pack()
 		
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="OK", background= "green")
@@ -23,12 +25,21 @@ class MyApp:
 		# Do not change <Button-1> when you create Button 2 :)
 		self.button1.bind("<Button-1>", self.button1Click) ### (1)
 		
+		self.button2 = Button(self.myContainer2)
+		self.button2.configure(text="Cancle", background= "red")
+		self.button2.grid(row=1, column=0)	
+		self.button2.bind("<Button-2>", self.button2Click) ### (2)
 		
 	def button1Click(self, event):    ### (3)
 		if self.button1["background"] == "green": ### (4)
 			self.button1["background"] = "yellow"
 		else:
 			self.button1["background"] = "green"
+	def button2Click(self, event):    ### (5)
+		if self.button2["background"] == "red": ### (6)
+			self.button2["background"] = "blue"
+		else:
+			self.button2["background"] = "red"
 	
 		
 root = Tk()
